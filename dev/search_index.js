068d64401d2703dd11563659df695c9129e4c596
Documenter.jl <documenter@juliadocs.github.io>
2025-04-07 05:43:21 +0000

    build based on 88e169a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HeartRateLab","category":"page"},{"location":"#HeartRateLab","page":"Home","title":"HeartRateLab","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HeartRateLab.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HeartRateLab]","category":"page"}]
+}
