036bf5fcb270ee9b4787862628bbabebe688277d
Alberto Barradas <abcsds@gmail.com>
2025-04-25 19:04:40 +0200

    Formatter

--- src/Preprocessing.jl
+++ src/Preprocessing.jl
@@ -156,21 +156,13 @@ function interpolate_nans!(n::Array{Float64,1}; method::Symbol=:linear)
     x = Float64.(collect(1:length(n)))
     # Select interpolation method
     if method == :constant
-        itp = DataInterpolations.ConstantInterpolation(
-            x[valid_indices], valid_values
-        )
+        itp = DataInterpolations.ConstantInterpolation(x[valid_indices], valid_values)
     elseif method == :linear
-        itp = DataInterpolations.LinearInterpolation(
-            x[valid_indices], valid_values
-        )
+        itp = DataInterpolations.LinearInterpolation(x[valid_indices], valid_values)
     elseif method == :quadratic
-        itp = DataInterpolations.QuadraticInterpolation(
-            x[valid_indices], valid_values
-        )
+        itp = DataInterpolations.QuadraticInterpolation(x[valid_indices], valid_values)
     elseif method == :cubic
-        itp = DataInterpolations.CubicSpline(
-            x[valid_indices], valid_values
-        )
+        itp = DataInterpolations.CubicSpline(x[valid_indices], valid_values)
     else
         throw(ArgumentError("Unsupported interpolation method: $method"))
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-import HeartRateLab
+using HeartRateLab: HeartRateLab
 using Test
 using Statistics
 
@@ -48,14 +48,22 @@ using Statistics
         @testset "replace_zeros" begin
             # Test replacing zeros with NaN
             @testset "Synthetic" begin
-                @test isequal(HeartRateLab.Preprocessing.replace_zeros([1, 2, 0, 4, 5, 0, 7]),Float64[1.0, 2.0, NaN, 4.0, 5.0, NaN, 7.0])
+                @test isequal(
+                    HeartRateLab.Preprocessing.replace_zeros([1, 2, 0, 4, 5, 0, 7]),
+                    Float64[1.0, 2.0, NaN, 4.0, 5.0, NaN, 7.0],
+                )
             end
         end
 
         @testset "replace_bio_outliers" begin
             # Test replacing biological outliers with NaN
             @testset "synthetic" begin
-                @test isequal(HeartRateLab.Preprocessing.replace_bio_outliers([400, 500, 200, 2000, 1000, 3000, 1500, 100]),Float64[400.0, 500.0, NaN, 2000, 1000.0, NaN, 1500.0, NaN])
+                @test isequal(
+                    HeartRateLab.Preprocessing.replace_bio_outliers([
+                        400, 500, 200, 2000, 1000, 3000, 1500, 100
+                    ]),
+                    Float64[400.0, 500.0, NaN, 2000, 1000.0, NaN, 1500.0, NaN],
+                )
             end
         end
 
@@ -70,7 +78,12 @@ using Statistics
                 ridx = [e<q_low || e>q_high for e in a_dist]
                 goal = copy(a_dist)
                 goal[ridx] .= repeat([NaN], sum(ridx))
-                @test isequal(HeartRateLab.Preprocessing.replace_statistical_outliers(a_dist; low=0.25, high=0.75),goal)
+                @test isequal(
+                    HeartRateLab.Preprocessing.replace_statistical_outliers(
+                        a_dist; low=0.25, high=0.75
+                    ),
+                    goal,
+                )
             end
         end
 
@@ -78,23 +91,74 @@ using Statistics
             # Test replacing ectopic beats with NaN
             # TODO
             @testset "rr-interval-healthy-subjects" begin
-                
                 hrv = HeartRateLab.Input.read_txt("testdata/example.txt")
                 n = hrv[1:1000]
                 @test sum(isnan.(n))==0
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:malik)))==0
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:kamath)))==0
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:acar, threshold=0.2)))==3
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:karlsson, threshold=0.2)))==499
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:custom, threshold=0.2)))==0
-    
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:malik)
+                    ),
+                )==0
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:kamath)
+                    ),
+                )==0
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(
+                            n; method=:acar, threshold=0.2
+                        ),
+                    ),
+                )==3
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(
+                            n; method=:karlsson, threshold=0.2
+                        ),
+                    ),
+                )==499
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(
+                            n; method=:custom, threshold=0.2
+                        ),
+                    ),
+                )==0
+
                 n = hrv[1000:2000]
                 @test sum(isnan.(n))==0
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:malik)))==0
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:kamath)))==0
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:acar, threshold=0.2)))==8
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:karlsson, threshold=0.2)))==500
-                @test sum(isnan.(HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:custom, threshold=0.2)))==0
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:malik)
+                    ),
+                )==0
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(n; method=:kamath)
+                    ),
+                )==0
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(
+                            n; method=:acar, threshold=0.2
+                        ),
+                    ),
+                )==8
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(
+                            n; method=:karlsson, threshold=0.2
+                        ),
+                    ),
+                )==500
+                @test sum(
+                    isnan.(
+                        HeartRateLab.Preprocessing.replace_ectopic_beats(
+                            n; method=:custom, threshold=0.2
+                        ),
+                    ),
+                )==0
             end
         end
 
@@ -110,20 +174,60 @@ using Statistics
         @testset "interpolate_nans" begin
             # Test interpolating NaN values
             @testset "synthetic" begin
-                @test isequal(HeartRateLab.Preprocessing.interpolate_nans(Float64[1, 2, NaN, 4, 5, NaN, 7.0]; method=:constant),Float64[1.0, 2.0, 2.0, 4.0, 5.0, 5.0, 7.0])
-                @test isequal(HeartRateLab.Preprocessing.interpolate_nans(Float64[1, 2, NaN, 4, 5, NaN, 7.0]; method=:linear),Float64[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
-                @test isequal(HeartRateLab.Preprocessing.interpolate_nans(Float64[1, 2, NaN, 4, 5, NaN, 7.0]; method=:quadratic),Float64[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
-                @test isequal(HeartRateLab.Preprocessing.interpolate_nans(Float64[1, 2, NaN, 4, 5, NaN, 7.0]; method=:cubic),Float64[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0])    
+                @test isequal(
+                    HeartRateLab.Preprocessing.interpolate_nans(
+                        Float64[1, 2, NaN, 4, 5, NaN, 7.0]; method=:constant
+                    ),
+                    Float64[1.0, 2.0, 2.0, 4.0, 5.0, 5.0, 7.0],
+                )
+                @test isequal(
+                    HeartRateLab.Preprocessing.interpolate_nans(
+                        Float64[1, 2, NaN, 4, 5, NaN, 7.0]; method=:linear
+                    ),
+                    Float64[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0],
+                )
+                @test isequal(
+                    HeartRateLab.Preprocessing.interpolate_nans(
+                        Float64[1, 2, NaN, 4, 5, NaN, 7.0]; method=:quadratic
+                    ),
+                    Float64[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0],
+                )
+                @test isequal(
+                    HeartRateLab.Preprocessing.interpolate_nans(
+                        Float64[1, 2, NaN, 4, 5, NaN, 7.0]; method=:cubic
+                    ),
+                    Float64[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0],
+                )
             end
         end
 
         @testset "Windowed" begin
             # Test windowed function
-            @testset "synthetic" begin                
+            @testset "synthetic" begin
                 v = Float64[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-                @test isequal(HeartRateLab.Preprocessing.windowed(v; window_size=3), [[1., 2., 3.], [2., 3., 4.], [3., 4., 5.], [4., 5., 6.], [5., 6., 7.], [6., 7., 8.], [7., 8., 9.], [8., 9., 10.]])
-                @test isequal(HeartRateLab.Preprocessing.windowed(v; window_size=3, stride=3), [[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]])
-                @test isequal(HeartRateLab.Preprocessing.windowed(v; window_size=3, stride=3, f=Statistics.mean), [2., 5., 8.])
+                @test isequal(
+                    HeartRateLab.Preprocessing.windowed(v; window_size=3),
+                    [
+                        [1.0, 2.0, 3.0],
+                        [2.0, 3.0, 4.0],
+                        [3.0, 4.0, 5.0],
+                        [4.0, 5.0, 6.0],
+                        [5.0, 6.0, 7.0],
+                        [6.0, 7.0, 8.0],
+                        [7.0, 8.0, 9.0],
+                        [8.0, 9.0, 10.0],
+                    ],
+                )
+                @test isequal(
+                    HeartRateLab.Preprocessing.windowed(v; window_size=3, stride=3),
+                    [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]],
+                )
+                @test isequal(
+                    HeartRateLab.Preprocessing.windowed(
+                        v; window_size=3, stride=3, f=Statistics.mean
+                    ),
+                    [2.0, 5.0, 8.0],
+                )
             end
         end
     end
